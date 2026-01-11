008d5a4af479653cd6c721d3e5daa6b74c0a7075
bryan-ignacio <bryan-ignacio@outlook.com>
2026-01-11 07:35:22 -0600

    input, button and list added.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,59 @@
+import { useState } from "react";
+
+/*
+{
+id: 1,
+title: "nueva tarea",
+done: true
+}
+*/
+let nextId = 0;
+
 export default function App() {
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleChange(e) {
+    setText(e.target.value);
+  }
+
+  function handleClick() {
+    let newTodos = [...todos];
+    if (text !== "") {
+      newTodos = [...todos, { id: nextId++, title: text, done: false }];
+    }
+    setTodos(newTodos);
+    setText("");
+  }
+
   return (
     <>
-      <h1>Todo App</h1>
-      <div>
-        <input type="text" />
-        <button>+</button>
-      </div>
-      <div>
-        <ul></ul>
-      </div>
+      <section className="container-app">
+        <h1 className="title">Todo App</h1>
+        <input
+          className="input"
+          type="text"
+          value={text}
+          onChange={handleChange}
+        />
+        <button className="button" onClick={handleClick}>
+          +
+        </button>
+        <div className="container-list">
+          <ul className="list">
+            {todos.map((todo) => {
+              return (
+                <li className="item" key={todo.id}>
+                  <input type="checkbox" />
+                  <label>{todo.title}</label>
+                  <button>Editar</button>
+                  <button>Eliminar</button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </section>
     </>
   );
 }
