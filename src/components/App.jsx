938d9c810676b73a94eb51cc26e524a93593997c
bryan-ignacio <bryan-ignacio@outlook.com>
2026-01-10 22:25:46 -0600

    start

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+    <>
+      <h1>Todo App</h1>
+      <div>
+        <input type="text" />
+        <button>+</button>
+      </div>
+      <div>
+        <ul></ul>
+      </div>
+    </>
+  );
+}
